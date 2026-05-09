81b39962e09bbb97bedbbd9992ab234339ee5f09
Dilan Torres <torresmottadilanstiven@gmail.com>
2026-05-09 15:51:52 -0500

    Delete categories-api directory

--- categories-api/src/categories/dto/create-category.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateCategoryDto {
-  @ApiProperty({ example: 'Electronics', description: 'Name of the category' })
-  name: string;
-}
